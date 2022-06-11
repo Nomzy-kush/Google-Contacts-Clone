53ab7cad081db915f36ff2106226efc12bce87fe
Onome <78811001+Nomzy-kush@users.noreply.github.com>
2022-06-11 13:19:35 -0800

    first commit

---
+++ my-app/src/Components/Home.js
@@ -0,0 +1,20 @@
+import React from 'react'
+// import Navbar from './Navbar'
+// import Menu from './Menu'
+import CreateContact from './CreateContact'
+
+
+const Home = () => {
+
+
+
+  return (
+    <>
+    {/* <Navbar />
+    <Menu /> */}
+    <CreateContact />
+    </>  
+  )
+}
+
+export default Home
